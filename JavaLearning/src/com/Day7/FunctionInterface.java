104c157cf4087113c605cc6b346a19160908ffed
Jyotsana15-code <jyotsanapriya2006>
2025-11-06 09:04:44 +0530

    files added

---
+++ JavaLearning/src/com/Day7/FunctionInterface.java
@@ -0,0 +1,11 @@
+package com.Day7;
+
+import java.util.function.Function;
+
+public class FunctionInterface {
+    public static void main(String[] args) {
+        Function<String,Integer> f= s->s.length();
+        System.out.println(f.apply("Hello World!"));
+    }
+
+}
